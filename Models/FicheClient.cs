253650d5d2e523233f8d4b4c69290a6379a923ff
amilkardev <melek.ferhi@gmail.com>
2021-03-12 17:01:19 +0100

    ficheClient

---
+++ Models/FicheClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autoveille.Models
+{
+    public class FicheClient
+    {
+        public int Id { get; set; }
+        public string NomClient { get; set; }
+        public string Langue { get; set; }
+        public string Courriel { get; set;}
+        public string Ville { get; set; }
+        public string  NoClient { get; set; }
+        public string Niv { get; set; }
+        public string  TypeAchat { get; set; }
+        public DateTime DateAchat { get; set; }
+        public string  Terme { get; set; }
+        public string  ValeurVehicule { get; set; }
+        public string VehiculeDesire { get; set; }
+        public string NoteAppelsPrecedents { get; set; }
+
+
+    }
+}
